e8d3ff1e3abd25a35299d9b4f0572c3c6e090e0b
Abineshik <62824687+Abineshik@users.noreply.github.com>
2020-09-25 09:15:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,38 +7,6 @@ import numpy as np
 import pandas as pd
 app = Flask(__name__)
 
-filename = 'regressor.pkl'
-model = pickle.load(open(filename, 'rb'))
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-@app.route('/back')
-def back():
-    return render_template('index.html')
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        print('Hey there')
-#    input_data = jsonify(request.form.to_dict())
-    #print('Ip is this')
-    input_data = request.form.to_dict()
-    input_df = pd.DataFrame(input_data, index=[0])
-    input_df = datatype_converter(input_df)
-    prediction = model.predict(input_df)
-    pred = np.round(prediction[0], 2)
-    input_data['Prediction'] = str(pred)
-    #input_data = jsonify(input_data)
-    #print(input_data)
-    return render_template('result.html', output_data = input_data, pred = pred)
-    #return str(prediction[0])
-
-#@app.route('/download', methods=['POST'])
-#def download():
-#    print('check')
-#    pass
-
-
 from sklearn.base import BaseEstimator, TransformerMixin
 
 def datatype_converter(df):
@@ -79,5 +47,39 @@ class OutlierClipping(BaseEstimator, TransformerMixin):
     df.loc[:,'condition'] = df.loc[:,'condition'].clip(lower=2)
     df.loc[:,'quality'] = df.loc[:,'quality'].clip(lower=5, upper=11)
     return df[['room_bed', 'room_bath', 'ceil']]
+
+filename = 'regressor.pkl'
+model = pickle.load(open(filename, 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/back')
+def back():
+    return render_template('index.html')
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        print('Hey there')
+#    input_data = jsonify(request.form.to_dict())
+    #print('Ip is this')
+    input_data = request.form.to_dict()
+    input_df = pd.DataFrame(input_data, index=[0])
+    input_df = datatype_converter(input_df)
+    prediction = model.predict(input_df)
+    pred = np.round(prediction[0], 2)
+    input_data['Prediction'] = str(pred)
+    #input_data = jsonify(input_data)
+    #print(input_data)
+    return render_template('result.html', output_data = input_data, pred = pred)
+    #return str(prediction[0])
+
+#@app.route('/download', methods=['POST'])
+#def download():
+#    print('check')
+#    pass
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
